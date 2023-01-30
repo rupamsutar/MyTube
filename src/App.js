88d554467db859b38e49d2a177cc383248a71661
Rupam Sutar <rupamsutar281998@gmail.com>
2023-01-30 18:38:22 +0530

    Routes setup completed

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from "@mui/material";
+
+import Navbar from './Components/Navbar';
+import Feed from './Components/Feed';
+import VideoDetail from './Components/VideoDetail';
+import ChannelDetail from './Components/ChannelDetail';
+import SearchFeed from './Components/SearchFeed';
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+      <BrowserRouter>
+        <Box sx={{backgroundColor: "#000"}}>
+          <Navbar />
+          <Routes>
+            <Route path='/' exact element={<Feed />}></Route>
+            <Route path='/video/:id' exact element={ <VideoDetail /> }></Route>
+            <Route path='/channel/:id' exact element={ <ChannelDetail />}></Route>
+            <Route path='/search/:searchTerm' exact element={ <SearchFeed /> }></Route>
+          </Routes>
+        </Box>
+      </BrowserRouter>
+    </>
   )
 }
 
