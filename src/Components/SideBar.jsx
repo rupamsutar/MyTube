622f7b125c775a59fd037b5df2434d114c0b0617
Rupam Sutar <rupamsutar281998@gmail.com>
2023-01-31 14:49:34 +0530

    Categories added in the sidebar

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import { Stack } from '@mui/material';
+import { categories } from '../utils/constants';
+
+const SelectedCategory = 'New';
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
+    <Stack direction='row' sx={{
+        overflowY: 'auto',
+        height: { sx: 'auto', md: '95%'},
+        flexDirection: {md: 'column'}
+    }}>
+        {categories.map( (category) => (
+            <button className='category-btn' style={{
+                backgroundColor: category.name === SelectedCategory && '#FC1503',
+                color: '#fff'
+            }}>
+                <span>{category.icon}</span>
+                <span>{category.name}</span>
+            </button>
+        ))}
+
+    </Stack>
   )
 }
 
