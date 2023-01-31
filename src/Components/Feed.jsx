39d9e519677e4e7d644c4b3faff5a951c5dcafd2
Rupam Sutar <rupamsutar281998@gmail.com>
2023-01-31 15:44:32 +0530

    Layout of the feed completed.
    Onto the Api part

--- src/Components/Feed.jsx
+++ src/Components/Feed.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from "@mui/material";
-import SideBar from './SideBar';
+
+import { SideBar, Videos } from "../Components";
 
 const Feed = () => {
   return (
@@ -21,7 +22,15 @@ const Feed = () => {
           </Typography>
 
         </Box>
+        <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex: '2'}}>
 
+          <Typography variant='h4' fontWeight='bold' mb={2} sx={{color: 'white'}}>
+            New
+            <span style={{color: '#F31503'}}> Videos</span>
+          </Typography>
+
+          <Videos videos={[]}></Videos>
+        </Box>
     </Stack>
   )
 }
