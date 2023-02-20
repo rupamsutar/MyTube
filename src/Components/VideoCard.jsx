129a442a0fab4438806b2cf64abcae24a28d1384
Rupam Sutar <rupamsutar281998@gmail.com>
2023-02-20 22:24:26 +0530

    Channel Title added

--- src/Components/VideoCard.jsx
+++ src/Components/VideoCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import { Typography, Card, CardContent, CardMedia } from "@mui/material";
-import { CheckCircle } from '@mui/icons-material';
+import { Check, CheckCircle } from '@mui/icons-material';
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle} from "../utils/constants"; 
 
 const VideoCard = ({video: {id: {videoId}, snippet}}) => {
@@ -17,6 +17,12 @@ const VideoCard = ({video: {id: {videoId}, snippet}}) => {
             <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
               <Typography variant="subtitle1" fontWeight="bold" color="#fff"> 
                 {snippet?.title.slice(0,60) || demoVideoTitle.slice(0, 60)}
+              </Typography>
+            </Link>
+            <Link to={snippet?.channelId ? `/video/${snippet?.channelId}` : demoChannelUrl}>
+              <Typography variant="subtitle1" fontWeight="bold" color="#fff"> 
+                {snippet?.channelTitle.slice(0,60) || demoChannelTitle.slice(0, 60)}
+                <CheckCircle sx={{fontSize:"12", color:"gray", ml:"5px"}} />
               </Typography>
             </Link>
         </CardContent>
